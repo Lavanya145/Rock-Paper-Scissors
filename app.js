57f899e6a6f7662e08d267f02a9537694ed169d1
lavanya <lavanyaaradhya@outlook.com>
2016-08-21 00:18:14 -0700

    Wins, losses and draws are stored

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 var objectsArray = ['rock', 'paper', 'scissors'];
+var userWin=0, computerWin=0, userLoss=0, computerLoss=0, userDraw=0,computerDraw = 0;
+
 function calculateWinner(computerChoice, userChoice)	{
-	if(userChoice == computerChoice)	
-			return "Draw!";
+	if(userChoice == computerChoice){
+			userDraw++;
+			computerDraw++;
+			return "Draw!";		
+	}	
 	else {
 		if(computerChoice == 'rock' && userChoice == 'paper')	{
+			userWin++;
+			computerLoss++;
 			return "You win!";					
 		}
 		if(computerChoice == 'rock' && userChoice == 'scissors')	{
+			computerWin++;
+			userLoss++;
 			return "Computer wins!";
 		}
 		if(computerChoice == 'paper' && userChoice == 'rock')	{
+			computerWin++;
+			userLoss++;
 			return "Computer wins!";
 		}
 		if(computerChoice == 'paper' && userChoice == 'scissors')	{
+			userWin++;
+			computerLoss++;
 			return "You win!";
 		}
 		if(computerChoice == 'scissors' && userChoice == 'paper')	{
+			computerWin++;
+			userLoss++;
 			return "Computer wins!";
 		}
 		if(computerChoice == 'scissors' && userChoice == 'rock')	{
+			userWin++;
+			computerLoss++;
 			return "You win!";
 		}
 	}
